02f357c3212ee4c77cb94c666dd76513ae972642
Timothy Riffe <timothy.riffe@unified-software-ai.com>
2025-01-28 17:35:03 -0800

    got rid of the src directory

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+// pages/_app.tsx
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from 'next/app';
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  // Enable dark mode in class-based approach
+  darkMode: "class",
+  
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
